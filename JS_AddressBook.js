4af59560c5aedd3eb44f70c614ee123376492c86
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-16 21:04:07 +0530

    [Anjaneya]added:Creating address book

--- JS_AddressBook.js
+++ JS_AddressBook.js
@@ -1,1 +1,14 @@
 console.log("Welcome to JavaScript AddressBook system");
+//UC1-Creating address book contact
+class AddressBook{
+constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phoneNumber = params[6];
+    this.email = params[7];
+  }
+}
